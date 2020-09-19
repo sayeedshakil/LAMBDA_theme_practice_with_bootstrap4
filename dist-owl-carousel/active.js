c1a989106654c834067af823ca598b4074254636
shakil <shakilot18@gmail.com>
2020-09-19 19:50:27 +0600

    first commit

---
+++ dist-owl-carousel/active.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:1000,
+    
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+
+
+
